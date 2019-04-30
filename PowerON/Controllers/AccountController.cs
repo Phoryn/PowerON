4eac3636fed9dd404be6b8c716f14d3f42c6eb88
Phoryn <mateusz@duda.pro>
2019-04-30 12:48:39 +0200

    Dodanie identity z tutka

---
+++ PowerON/Controllers/AccountController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using PowerON.ViewModel;
+
+namespace PowerON.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.ReturnUrl = returnUrl;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model, string returnUrl)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            else
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+        }
+
+
+
+
+    }
+}
---
+++ PowerON/ViewModel/AccountViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerON.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Wprowadź adres e-mail")]
+        [EmailAddress]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Wprowadź hasło")]
+        public string Password { get; set; }
+        [Display(Name = "Pamiętaj mnie!")]
+        public bool RememberMe { get; set; }
+    }
+
+    public class RegisterViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [StringLength(100,ErrorMessage = "{0} musi być dłuższe od {2}.", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Hasło")]
+        public string Password { get; set; }
+
+
+
+        [DataType(DataType.Password)]
+        [Display(Name = "Potwierdź Hasło")]
+        [Compare("Password", ErrorMessage = "Wpisane hasła nie pasują do siebie!" )]
+        public bool ConfirmPassword { get; set; }
+    }
+}
